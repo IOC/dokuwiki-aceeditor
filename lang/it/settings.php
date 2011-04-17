b6b90a1a04b5c0d46eb2517d8338280d470c1adb
Albert Gasset <albert.gasset@gmail.com>
2011-04-17 21:44:45 +0200

    italian translation by vinnie

---
+++ lang/it/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['colortheme'] = "Tema Colori";
+$lang['colortheme_o_dark'] = "Scuro";
+$lang['colortheme_o_light'] = "Chiaro";
+$lang['highlight'] = 'Abilita colorazione sintassi';
+$lang['latex'] = 'Abilita il supporto al LaTeX plugin';
+$lang['wraplimit'] = 'Limite a capo automatico';
+$lang['loadjquery'] = 'Carica librerie JQuery';
