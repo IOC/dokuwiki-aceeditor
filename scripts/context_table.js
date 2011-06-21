75712274c468eec3ea3fd82ed29222902b59e4ef
Albert Gasset <albert.gasset@gmail.com>
2011-06-21 18:38:33 +0200

    context_table: support for colspan

--- scripts/context_table.js
+++ scripts/context_table.js
@@ -21,56 +21,93 @@ define(function(require) {
     return function(spec) {
         var that = {};
 
-        var new_cell = function(is_header, align, content) {
+        var new_cell = function(spec) {
             var that = {};
 
-            that.set_align = function(value) {
-                align = value;
+            var trimmed_content = function() {
+                return spec.content.replace(/^ +/, "").replace(/ +$/, "");
+            };
+
+            var update_layout = function(layout, offset) {
+                var i, min_length, space, target_length = 0;
+                var padding = {left: 1, right: 1};
+
+                if (spec.align === "center") {
+                    padding.left = padding.right = 2;
+                } else if (spec.align === "right") {
+                    padding.left = 2;
+                }
+
+                min_length = (trimmed_content().length + spec.colspan +
+                              padding.left + padding.right);
+
+                for (i = offset; i < offset + spec.colspan; i += 1) {
+                    layout[i] = layout[i] || 0;
+                    target_length += layout[i];
+                }
+
+                if (min_length < target_length) {
+                    space = target_length - min_length;
+                    if (spec.align === "center") {
+                        padding.left += Math.floor(space / 2);
+                        padding.right += Math.ceil(space / 2);
+                    } else if (spec.align === "right") {
+                        padding.left += space;
+                    } else {
+                        padding.right += space;
+                    }
+                } else {
+                    space = min_length - target_length;
+                    for (i = 0; i < spec.colspan; i += 1) {
+                        layout[offset + i] += Math.floor(space / spec.colspan);
+                    }
+                    for (i = 0; i < space % spec.colspan; i += 1) {
+                        layout[offset + i] += 1;
+                    }
+                }
+
+                return padding;
             };
 
             that.cursor_position = function() {
-                return 1 + Math.max(1, content.replace(/ +$/, "").length);
+                return 1 + Math.max(1, spec.content.replace(/ +$/, "").length);
             };
 
-            that.format = function(length) {
-                var padding, padding_left, padding_right;
+            that.colspan = function() {
+                return spec.colspan;
+            };
 
-                content = content.replace(/^ +/, "").replace(/ +$/, "");
-                padding = length - content.length - 1;
-
-                if (align === "right") {
-                    padding = Math.max(3, padding);
-                    padding_left = padding - 1;
-                    padding_right = 1;
-                } else if (align === "center") {
-                    padding = Math.max(4, padding);
-                    padding_left = Math.floor(padding / 2);
-                    padding_right = Math.ceil(padding / 2);
-                } else {
-                    padding = Math.max(2, padding);
-                    padding_left = 1;
-                    padding_right = padding - 1;
+            that.format = function(layout, offset, pass) {
+                var padding;
+                if (pass >= 2 || spec.colspan === 1) {
+                    padding = update_layout(layout, offset);
                 }
-
-                content = (new Array(padding_left + 1).join(" ") +
-                           content +
-                           new Array(padding_right + 1).join(" "));
+                if (pass >= 3) {
+                    spec.content = (new Array(padding.left + 1).join(" ") +
+                                    trimmed_content() +
+                                    new Array(padding.right + 1).join(" "));
+                }
             };
 
             that.is_header = function() {
-                return is_header;
+                return spec.is_header;
             };
 
             that.length = function() {
-                return 1 + content.length;
+                return 1 + spec.content.length;
             };
 
             that.toggle_header = function() {
-                is_header = !is_header;
+                spec.is_header = !spec.is_header;
+            };
+
+            that.set_align = function(value) {
+                spec.align = value;
             };
 
             that.value = function() {
-                return (is_header ? "^" : "|") + content;
+                var sep = spec.is_header ? "^" : "|";
+                return sep + spec.content + new Array(spec.colspan).join(sep);
             };
 
             return that;
@@ -82,6 +119,14 @@ define(function(require) {
             that.align_cell = function(index, align) {
                 cells[index].set_align(align);
             };
+
+            that.columns = function() {
+                var i, result = 0;
+                for (i = 0; i < cells.length; i += 1) {
+                    result += cells[i].colspan();
+                }
+                return result;
+            }
 
             that.cursor_position = function(cell) {
                 var i, position = 0;
@@ -102,6 +147,21 @@ define(function(require) {
                 return cells.length - 1;
             };
 
+            that.fill = function(columns) {
+                var i, is_header = false;
+                if (cells.length > 0) {
+                    is_header = cells[cells.length - 1].is_header();
+                }
+                for (i = that.columns(); i < columns; i += 1) {
+                    cells.push(new_cell({
+                        align: "left",
+                        colspan: 1,
+                        content: "  ",
+                        is_header: is_header
+                    }));
+                }
+            };
+
             that.move_cell_left = function(index) {
                 if (index > 0 && index < cells.length) {
                     cells.splice(index - 1, 2, cells[index], cells[index - 1]);
@@ -114,17 +174,11 @@ define(function(require) {
                 }
             };
 
-            that.format = function(column_lengths) {
-                var i, is_header;
+            that.format = function(layout, pass) {
+                var i, offset = 0;
                 for (i = 0; i < cells.length; i += 1) {
-                    column_lengths[i] = column_lengths[i] || 0;
-                    cells[i].format(column_lengths[i]);
-                    column_lengths[i] = Math.max(column_lengths[i], cells[i].length());
-                };
-                is_header = cells.length > 0 ? cells[cells.length - 1].is_header() : false;
-                for (i = cells.length; i < column_lengths.length; i += 1) {
-                    cells.push(new_cell(is_header, "left", ""));
-                    cells[i].format(column_lengths[i]);
+                    cells[i].format(layout, offset, pass);
+                    offset += cells[i].colspan();
                 };
             };
 
@@ -165,12 +219,23 @@ define(function(require) {
             };
 
             var format = function() {
-                var i, column_lengths = [], lines = [];
+                var i, layout = [], lines = [], pass;
+                normalize();
+                for (pass = 1; pass <= 3; pass += 1) {
+                    for (i = 0; i < rows.length; i += 1) {
+                        rows[i].format(layout, pass);
+                    }
+                }
+                update();
+            };
+
+            var normalize = function() {
+                var columns = 0, i;
                 for (i = 0; i < rows.length; i += 1) {
-                    rows[i].format(column_lengths);
+                    columns = Math.max(columns, rows[i].columns());
                 }
                 for (i = 0; i < rows.length; i += 1) {
-                    rows[i].format(column_lengths);
+                    rows[i].fill(columns);
                 }
             };
 
@@ -186,31 +251,30 @@ define(function(require) {
             that.align_cell = function(align) {
                 rows[cursor_row].align_cell(cursor_cell, align);
                 format();
-                update();
             };
 
             that.move_column_left = function() {
                 var i;
-                format();
+                normalize();
                 if (cursor_cell > 0) {
                     for (i = 0; i < rows.length; i += 1) {
                         rows[i].move_cell_left(cursor_cell);
                     }
                     cursor_cell -= 1;
                 }
-                update();
+                format();
             };
 
             that.move_column_right = function() {
                 var i;
-                format();
+                normalize();
                 if (cursor_cell < rows[cursor_row].length() - 1) {
                     for (i = 0; i < rows.length; i += 1) {
                         rows[i].move_cell_right(cursor_cell);
                     }
                     cursor_cell += 1;
                 }
-                update();
+                format();
             };
 
             that.next_cell = function() {
@@ -223,7 +287,6 @@ define(function(require) {
                     }
                 }
                 format();
-                update();
             };
 
             that.next_row = function() {
@@ -232,7 +295,6 @@ define(function(require) {
                     rows.push(new_row([]));
                 }
                 format();
-                update();
             };
 
             that.previous_cell = function() {
@@ -243,7 +305,6 @@ define(function(require) {
                     cursor_cell = rows[cursor_row].length() - 1;
                 }
                 format();
-                update();
             };
 
             that.previous_row = function() {
@@ -251,12 +312,11 @@ define(function(require) {
                     cursor_row -= 1;
                 }
                 format();
-                update();
             };
 
             that.remove_column = function() {
                 var i;
-                format();
+                normalize();
                 if (rows[0].length() > 1) {
                     for (i = 0; i < rows.length; i += 1) {
                         rows[i].remove_cell(cursor_cell);
@@ -265,53 +325,66 @@ define(function(require) {
                         cursor_cell -= 1;
                     }
                 }
-                update();
+                format();
             };
 
             that.toggle_header = function() {
                 rows[cursor_row].toggle_header(cursor_cell);
-                update();
+                format();
             };
 
             return that;
         };
 
-        var parse_row = function(row, column) {
-            var cells = [], i, is_header, value;
-            var tokens = spec.ace.get_tokens(row);
+        var parse_row = function(row) {
+            var cells = [], content = null, i, is_header, tokens;
 
-            if (tokens.length === 0 || tokens[0].type !== "table") {
-                return;
-            }
-
-            var push_cell = function() {
+            var push_cell = function(colspan) {
                 var align;
-                if (value !== undefined) {
-                    align = (/^  +[^ ]/.test(value) ?
-                             (/[^ ] + $/.test(value) ? "center" : "right") :
+                if (content !== null) {
+                    align = (/^  +[^ ]/.test(content) ?
+                             (/[^ ] + $/.test(content) ? "center" : "right") :
                              "left");
-                    cells.push(new_cell(is_header, align, value));
+                    cells.push(new_cell({
+                        align: align,
+                        colspan: colspan,
+                        content: content,
+                        is_header: is_header
+                    }));
                 }
             }
 
             var parse_table_token = function(token) {
-                var i, align;
+                var i, colspan;
+
+                var is_separator = function(i) {
+                    return token[i] === "|" || token[i] === "^";
+                }
+
                 for (i = 0; i < token.length; i += 1) {
-                    if (token[i] === "|" || token[i] === "^") {
-                        push_cell();
+                    if (is_separator(i)) {
+                        for (colspan = 1; is_separator(i + 1); colspan += 1) {
+                            i += 1;
+                        }
+                        push_cell(colspan);
                         is_header = (token[i] === "^");
-                        value = "";
+                        content = "";
                     } else {
-                        value += token[i];
+                        content += token[i];
                     }
                 }
             };
+
+            tokens = spec.ace.get_tokens(row);
+            if (tokens.length === 0 || tokens[0].type !== "table") {
+                return;
+            }
 
             for (i = 0; i < tokens.length; i += 1) {
                 if (tokens[i].type === "table") {
                     parse_table_token(tokens[i].value);
                 } else {
-                    value += tokens[i].value;
+                    content += tokens[i].value;
                 }
             }
 
