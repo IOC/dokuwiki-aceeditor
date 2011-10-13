d58b451ff01147056368a6a88e51734baabcad17
Albert Gasset <albert.gasset@gmail.com>
2011-10-13 11:43:58 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir=$(dirname $0)
+branch=$(git --git-dir=$dir/.git rev-parse --abbrev-ref HEAD)
+version=${1:-$branch}
+
+rm -rf $dir/build
+coffee -c $dir/scripts/*.coffee
+
+r.js -o name=main out=$dir/build/main.js baseUrl=$dir/scripts \
+    paths.requirejs=require include=requirejs \
+    paths.ace/requirejs/text=text name=main
+r.js -o baseUrl=. appDir=$dir/scripts/ace/theme dir=$dir/build/ace/theme
+rm -f $dir/build/ace/theme/build.txt
+
+tar -c -z -f $dir/aceeditor-$version.tar.gz -C $dir/.. \
+    aceeditor/{action.php,build,conf,images,lang,NEWS,plugin.info.txt,preview.php}
