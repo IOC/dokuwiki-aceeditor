712b4d0eb0d82b371ea5894d9d204402e352921e
Albert Gasset <albert.gasset@gmail.com>
2011-12-13 13:45:09 +0100

    Polish translation by Aleksander Setlak

---
+++ lang/pl/settings.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Polish language file
+ *
+ * @license GNU General Public License 3 <http://www.gnu.org/licenses/>
+ * @author Aleksander Setlak <http://alek.magazynek.org>
+ */
+ 
+$lang['colortheme'] = "Schemat kolorów";
+$lang['colortheme_o_clouds'] = "Clouds";
+$lang['colortheme_o_clouds_midnight'] = "Clouds Midnight";
+$lang['colortheme_o_cobalt'] = "Cobalt";
+$lang['colortheme_o_crimson_editor'] = "Crimson Editor";
+$lang['colortheme_o_dawn'] = "Dawn";
+$lang['colortheme_o_eclipse'] = "Eclipse";
+$lang['colortheme_o_idle_fingers'] = "idleFingers";
+$lang['colortheme_o_kr_theme'] = "krTheme";
+$lang['colortheme_o_merbivore'] = "Merbivore";
+$lang['colortheme_o_merbivore_soft'] = "Merbivore Soft";
+$lang['colortheme_o_mono_industrial'] = "Mono Industrial";
+$lang['colortheme_o_monokai'] = "Monokai";
+$lang['colortheme_o_pastel_on_dark'] = "Pastel on dark";
+$lang['colortheme_o_solarized_dark'] = "Solarized Dark";
+$lang['colortheme_o_solarized_light'] = "Solarized Light";
+$lang['colortheme_o_textmate'] = "TextMate";
+$lang['colortheme_o_tomorrow'] = "Tomorrow";
+$lang['colortheme_o_tomorrow_night'] = "Tomorrow Night";
+$lang['colortheme_o_tomorrow_night_blue'] = "Tomorrow Night Blue";
+$lang['colortheme_o_tomorrow_night_bright'] = "Tomorrow Night Bright";
+$lang['colortheme_o_tomorrow_night_eighties'] = "Tomorrow Night 80s";
+$lang['colortheme_o_twilight'] = "Twilight";
+$lang['colortheme_o_vibrant_ink'] = "Vibrant Ink";
+
+$lang['highlight'] = 'Włącz podświetlanie składni';
+$lang['latex'] = 'Włącz wsparcie dla wtyczki LaTeX';
+$lang['wraplimit'] = 'Limit zawijania tekstu';
+$lang['loadjquery'] = 'Ładuj bibliotekę jQuery';
